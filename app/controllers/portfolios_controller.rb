b97c080dade0321c2afaee1d3d74463fee62ccf4
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-07-17 10:50:40 -0600

    Implemented edit  and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,10 @@
 class PortfoliosController < ApplicationController
-	def index
-		@portfolio_items = Portfolio.all 
+    def index
+        @portfolio_items = Portfolio.all 
     end
 
     def new 
-    	@portfolio_item = Portfolio.new
+        @portfolio_item = Portfolio.new
     end
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -12,27 +12,28 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'You blog was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_item}
+        # format.json { render :show, status: :created, location: @portfolio_item}
       else
         format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+        # format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
-  end
-def edit
-  @portfolio_item = Portfolio.find(params[:id])
-  end
+  end  
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end 
 
   def update
-  @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to @portfolios_path, notice: 'The record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
+      if@portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'This record was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
