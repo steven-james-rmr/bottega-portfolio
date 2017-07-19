383ec01258058cc4f506d32c69a62b909b63bb69
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-07-19 15:09:26 -0600

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
-	include Placeholder
+  has_many :technologies
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
