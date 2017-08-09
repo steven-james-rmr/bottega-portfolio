b18c63c6d20f672c5858a2eb982f03e26cbb5998
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-08-09 15:05:09 -0600

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
-module ApplicationHelper	
+module ApplicationHelper
+  
   def login_helper style = '' 
 	  if current_user.is_a?(GuestUser)
        (link_to "Register", new_user_registration_path, class: style) +
@@ -51,12 +52,23 @@ module ApplicationHelper
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
-
-  nav_links.html_safe
+    nav_links.html_safe
   end
 
   def active? path
     "active" if current_page? path
-  end  
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Steven James Portfolio", time: 1000)
+  end
 
 end
