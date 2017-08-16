c940cb9f45bec4a57b67526eba182990e345602b
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-08-16 15:51:23 -0600

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module TechPortfolioT
   class Application < Rails::Application
   	config.eager_load_paths << "#{Rails.root}/lib"
     config.load_defaults 5.1
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
