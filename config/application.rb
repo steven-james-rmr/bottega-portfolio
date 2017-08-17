ce8ad28cc3ea35e52a5cd8ddd07837129e17bea1
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-08-17 10:34:20 -0600

    more stuff

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,5 @@ module TechPortfolioT
   	config.eager_load_paths << "#{Rails.root}/lib"
     config.load_defaults 5.1
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
-    config.serve_static_assets = true
   end
 end
