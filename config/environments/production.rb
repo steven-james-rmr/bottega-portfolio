501bd3c55456fb72d374709bc8ac6037e1608ae5
steven-james-rmr <rockymtnrednecks@gmail.com>
2017-09-28 13:15:29 -0600

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://snakecreek-dev.herokuapp.com', 'https://snakecreek-dev.herokuapp.com/']
+  config.action_cable.url = "wss://snakecreek-dev.herokuapp.com/cable"
 end
